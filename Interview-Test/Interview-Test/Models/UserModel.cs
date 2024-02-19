cf70ce52e0620a3134ca54344131eb9c0744f00d
SIGMAF <phongsirim@tatasteelthailand.com>
2024-02-19 14:03:10 +0700

    ส่งเทสครับ

--- Interview-Test/Interview-Test/Models/UserModel.cs
+++ Interview-Test/Interview-Test/Models/UserModel.cs
@@ -11,3 +11,19 @@ public class UserModel
     [Required]
     public ICollection<UserRoleMappingModel> UserRoleMappings { get; set; }
 }
+
+public class User
+{
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int? Age { get; set; }
+    public List<Role> Roles { get; set; }
+    public List<string> Permissions { get; set; }
+}
+public class Role
+{
+    public int RoleId { get; set; }
+    public string RoleName { get; set; }
+}
--- Interview-Test/Interview-Test/Repositories/Data.cs
+++ Interview-Test/Interview-Test/Repositories/Data.cs
@@ -1,4 +1,5 @@
 ﻿using Interview_Test.Models;
+using System.Data;
 
 namespace Interview_Test.Repositories;
 
@@ -146,6 +147,52 @@ public static class Data
                     }
                 }
             }
+        }
+    ];
+
+    public static List<User> _users =>
+    [
+       new User
+        {
+            Id = "user01",
+            Username = "John.D.Smith",
+            FirstName = "John",
+            LastName = "Smith",
+           Age = null,
+            Roles = new List<Role>
+            {
+            new Role { RoleId = 1, RoleName = "pick operation" },
+            new Role { RoleId = 2, RoleName = "pack operation" },
+            new Role { RoleId = 3, RoleName = "document operation" }
+            },
+            Permissions = [
+                "1-01-picking-info",
+                "1-02-picking-start",
+                "1-03-picking-confirm",
+                "1-04-picking-report",
+                "2-01-packing-info",
+                "2-02-packing-start",
+                "2-03-packing-confirm",
+                "2-04-packing-report",
+                "3-01-printing-label"
+            ]
+        },
+        new User
+        {
+           Id = "user02",
+            Username = "Bob.M.Jackson",
+            FirstName = "Bob",
+            LastName = "Jackson",
+            Age = 28,
+            Roles = new List<Role>
+            {
+            new Role { RoleId = 3, RoleName = "document operation" }
+            },
+            Permissions = [
+                "1-04-picking-report",
+                "2-04-packing-report",
+                "3-01-printing-label"
+            ]
         }
     ];
 }

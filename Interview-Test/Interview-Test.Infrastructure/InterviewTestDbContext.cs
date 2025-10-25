c7a25efa082768cd4687f2e20fde138e61635e8a
SILAKOB\silak <silakob2@gmail.com>
2025-10-26 00:25:44 +0700

    edit UserRoleMappingModel.

--- Interview-Test/Interview-Test.Infrastructure/InterviewTestDbContext.cs
+++ Interview-Test/Interview-Test.Infrastructure/InterviewTestDbContext.cs
@@ -15,6 +15,19 @@ public class InterviewTestDbContext : DbContext
     public DbSet<RoleModel> RoleTb { get; set; }
     public DbSet<UserRoleMappingModel> UserRoleMappingTb { get; set; }
     public DbSet<PermissionModel> PermissionTb { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserRoleMappingModel>(entity =>
+        {
+            entity.HasOne(urm => urm.User)
+                  .WithMany(u => u.UserRoleMappings)
+                  .HasForeignKey("UserId")
+                  .HasPrincipalKey(u => u.Id)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
 
 public class InterviewTestDbContextDesignFactory : IDesignTimeDbContextFactory<InterviewTestDbContext>
--- Interview-Test/Interview-Test.Infrastructure/Models/UserRoleMappingModel.cs
+++ Interview-Test/Interview-Test.Infrastructure/Models/UserRoleMappingModel.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Interview_Test.Models;
 
 [Table("UserRoleMappingTb")]
 public class UserRoleMappingModel
 {
-    [ForeignKey("Id")]
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid UserRoleMappingId { get; set; }
+    [ForeignKey("UserId")]
     public UserModel User { get; set; }
     [ForeignKey("RoleId")]
     public RoleModel Role { get; set; }
